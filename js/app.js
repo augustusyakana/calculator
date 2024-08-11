962a59ae991f6a5d4e8b0f154e9fac07f225cdbb
Augustus Yuki Yakana <131854470+augustusyakana@users.noreply.github.com>
2024-08-10 19:46:37 -1000

    added ui

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const calContainer = document.querySelector("#calculatorContainer");
+
+const words = document.createElement("div");
+const head = document.createElement("h1");
+head.textContent = "Wassup!";
+words.appendChild(head);
